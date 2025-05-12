8d8e0de8f61d0f1ed994f0ab08eb97e81ae18c65
James Dismore <jamesdismore@outlook.com>
2025-05-12 17:50:33 +0100

    space repo complete

--- lib/space_repository.py
+++ lib/space_repository.py
@@ -8,7 +8,7 @@ class SpaceRepository():
         rows = self._connection.execute('SELECT * from spaces')
         spaces = []
         for row in rows:
-            item = Space(row['id'].row['name'],row['description'],row['price'],row['host_id'])
+            item = Space(row['id'],row['name'],row['description'],row['price_per_night'],row['host_id'])
             spaces.append(item)
 
         return spaces
