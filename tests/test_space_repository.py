04b416ada2fc3267bd1636d111d0d91474fd2b68
James Dismore <jamesdismore@outlook.com>
2025-05-12 17:46:59 +0100

    fixed seed bugs

--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -2,7 +2,9 @@ from lib.space import *
 from lib.space_repository import *
 
 def test_get_all_spaces(db_connection):
-    #db_connection.seed('')
+    db_connection.seed('seeds/makersbnb_seed.sql')
     repository = SpaceRepository(db_connection)
 
-    assert repository.all() == []
+    assert repository.all() == [
+        Space(1,'The Barn','Converted barn set in a rural location', 65, 1)
+    ]
