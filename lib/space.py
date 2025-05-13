17ef1988171f0d0c8624e8b19e76d17152998343
Maddy Miller <maddyjmiller@outlook.com>
2025-05-13 13:27:07 +0100

    added name, description, ppn to html; make database TEST which'll need sorting; edited the Space.py file to ppn from price

--- lib/space.py
+++ lib/space.py
@@ -1,13 +1,13 @@
 class Space:
-    def __init__(self,id,name,description,price,host_id):
+    def __init__(self,id,name,description,price_per_night,host_id):
         self.id = id
         self.name = name
         self.description = description
-        self.price = price
+        self.price_per_night = price_per_night
         self.host_id = host_id
 
     def __repr__(self):
-        return f'Space({self.id}, {self.name}, {self.description}, £{self.price}/night, {self.host_id})'
+        return f'Space({self.id}, {self.name}, {self.description}, £{self.price_per_night}/night, {self.host_id})'
 
     def __eq__(self,other):
         return self.__dict__ == other.__dict__
