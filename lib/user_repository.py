87dc865b0030ed1ed0cb57802b9b5821c5b73e81
Tim Auton <tim@uton.org>
2025-05-13 15:40:04 +0100

    User Repository Class Created and Tested

---
+++ lib/user_repository.py
@@ -0,0 +1,29 @@
+from lib.user import User
+
+class UserRepository:
+    def __init__(self, connection):
+        self._connection = connection
+    
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM users')
+        return [
+            User(row['id'],row['user_name'],row['email'],row['phone']) for row in rows
+        ]
+
+    def create(self, user):
+        rows = self._connection.execute('INSERT INTO users ( user_name, email, phone ) VALUES ( %s, %s, %s )', [ user.user_name, user.email, user.phone ] )
+        return None
+    
+    def find(self, id):
+        rows = self._connection.execute('SELECT * FROM users WHERE id = %s', [ id ])
+        row = rows[0]
+        return User(row['id'],row['user_name'],row['email'],row['phone'])
+
+    def find_by_email(self, email):
+        rows = self._connection.execute('SELECT * FROM users WHERE email = %s', [ email ])
+        row = rows[0]
+        return User(row['id'],row['user_name'],row['email'],row['phone'])
+
+    def delete(self,id):
+        rows = self._connection.execute('DELETE FROM users WHERE id = %s', [ id ])
+        return None
