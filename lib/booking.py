4bd7eac50af359cbc527c3ddad8cdbffcea0cb29
James Dismore <jamesdismore@outlook.com>
2025-05-14 11:51:34 +0100

    fixed booking object

--- lib/booking.py
+++ lib/booking.py
@@ -1,12 +1,13 @@
 class Booking:
-    def __init__(self,id,start_date,end_date,user_id):
+    def __init__(self,id,start_date,end_date,space_id,user_id):
         self.id = id
         self.start_date = start_date
         self.end_date = end_date
+        self.space_id = space_id
         self.user_id = user_id
 
     def __repr__(self):
-        return f'Booking({self.id}, {self.start_date}, {self.end_date}, {self.user_id})'
+        return f'Booking({self.id}, {self.start_date}, {self.end_date}, {self.space_id}, {self.user_id})'
     
     def __eq__ (self,other):
         return self.__dict__ == other.__dict__
--- tests/test_booking.py
+++ tests/test_booking.py
@@ -1,22 +1,23 @@
 from lib.booking import Booking
 
 def test_booking_constructs():
-    booking = Booking(1,'2025-01-01','2026-01-01',1)
+    booking = Booking(1,'2025-01-01','2026-01-01',1,1)
 
     assert booking.id == 1
     assert booking.start_date == '2025-01-01'
     assert booking.end_date == '2026-01-01'
+    assert booking.space_id == 1
     assert booking.user_id == 1
 
 def test_booking_formatting():
-    booking = Booking(1,'2025-01-01','2026-01-01',1)
+    booking = Booking(1,'2025-01-01','2026-01-01',1,1)
 
-    assert str(booking) == 'Booking(1, 2025-01-01, 2026-01-01, 1)'
+    assert str(booking) == 'Booking(1, 2025-01-01, 2026-01-01, 1, 1)'
 
 def test_bookings_are_equal():
-    booking1 = Booking(1,'2025-01-01','2026-01-01',1)
-    booking2 = Booking(1,'2025-01-01','2026-01-01',1)
-    booking3 = Booking(2,'2025-01-01','2026-01-01',1)
+    booking1 = Booking(1,'2025-01-01','2026-01-01',1,1)
+    booking2 = Booking(1,'2025-01-01','2026-01-01',1,1)
+    booking3 = Booking(2,'2025-01-01','2026-01-01',1,1)
 
     assert booking1 == booking2
     assert booking1 != booking3
