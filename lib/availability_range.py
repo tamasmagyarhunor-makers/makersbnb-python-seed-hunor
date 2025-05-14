e65fea7f051b877fc69e7c18c77cf8fcf06be971
James Dismore <jamesdismore@outlook.com>
2025-05-14 11:58:13 +0100

    availability object done

--- lib/availability_range.py
+++ lib/availability_range.py
@@ -1,3 +1,12 @@
 class AvailabilityRange:
-    def __init__(self):
-        pass
+    def __init__(self,id,start_date,end_date,space_id):
+        self.id = id
+        self.start_date = start_date
+        self.end_date = end_date
+        self.space_id = space_id
+
+    def __repr__(self):
+        return f'AvailableRange({self.id}, {self.start_date}, {self.end_date}, {self.space_id})'
+    
+    def __eq__ (self,other):
+        return self.__dict__ == other.__dict__
