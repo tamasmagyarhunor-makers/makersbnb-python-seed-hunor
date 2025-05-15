7d021e954dc02be03ad9e3210ec1b938b1e70b11
James Dismore <jamesdismore@outlook.com>
2025-05-15 12:03:35 +0100

    james changes accidentally on main

--- lib/availability_range_repository.py
+++ lib/availability_range_repository.py
@@ -9,3 +9,19 @@ class AvailabilityRangeRepository():
         row = rows[0]
 
         return AvailabilityRange(row['id'],row['start_date'],row['end_date'],row['space_id'])
+    
+    def find_by_space_id(self,space_id):
+        rows = self._connection.execute('SELECT * FROM availability_ranges WHERE space_id = %s',[space_id])
+        avail_ranges_list = []
+        for row in rows:
+            item = AvailabilityRange(row['id'],row['start_date'],row['end_date'],row['space_id'])
+            avail_ranges_list.append(item)
+
+        return avail_ranges_list
+    
+    def add_range(self,start_date,end_date,space_id):
+        self._connection.execute('INSERT INTO availability_ranges (start_date,end_date,space_id) VALUES (%s,%s,%s)',
+                                [start_date,
+                                end_date,
+                                space_id])
+        
