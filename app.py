fa25281be74649bba4264c3ba87756318a6e1a6b
Ting <xueting.li1996@outlook.com>
2025-07-23 14:48:29 +0100

    add user routes with test and basic html templates

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
+from lib.user import *
+from lib.user_repository import *
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -14,6 +16,65 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+
+
+# new code below
+"""
+get all users
+"""
+@app.route('/users', methods=['GET'])
+def get_users():
+    try:
+        connection = get_flask_database_connection(app)
+        repository = UserRepository(connection)
+        users = repository.all()
+        return render_template('users/index.html', users=users)
+    except Exception as e:
+        return f"Database error: {e}"
+"""
+get a single user by id
+"""
+@app.route('/users/<int:id>', methods=['GET'])
+def show_user(id):
+    connection = get_flask_database_connection(app)
+    repository = UserRepository(connection)
+    user = repository.find(id)
+    return render_template('users/show.html', user=user)
+
+"""
+post a new user
+"""
+# GET /users/new
+# Returns a form to create a new book
+@app.route('/users/new', methods=['GET'])
+def get_new_user():
+    return render_template('users/new.html')
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    # Set up the database connection and repository
+    connection = get_flask_database_connection(app)
+    repository = UserRepository(connection)
+
+    # Get the fields from the request form
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+
+    # Create a user object
+    user = User(None, name, email, password)
+
+    # # Check for validity and if not valid, show the form again with errors
+    # if not user.is_valid():
+    #     return render_template('users/new.html', user=user, errors=user.generate_errors()), 400
+
+    # Save the book to the database
+    user = repository.create(user)
+
+    # Redirect to the book's show route to the user can see it
+    return redirect(f"/users/{user.id}")
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,79 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(p_tag).to_have_text("This is the homepage.")
+
+
+
+"""
+Get all the users
+"""
+def test_get_users(db_connection, page, test_web_address):
+    db_connection.seed("seeds/makers_bnb.sql")
+
+    # We load a virtual browser and navigate to the /books page
+    page.goto(f"http://{test_web_address}/users")
+
+    # We look at all the <li> tags
+    list_items = page.locator("li")
+
+    # We assert that it has the users in it
+    expect(list_items).to_have_text([
+        "Alice (alice@example.com)",
+        "Bob (bob@example.com)"
+    ])
+
+
+"""
+Get a single user
+"""
+def test_get_user(db_connection, page, test_web_address):
+    db_connection.seed("seeds/makers_bnb.sql")
+
+    # We visit the books page
+    page.goto(f"http://{test_web_address}/users")
+
+    # Click the link with the text 'Name: Alice Email: alice@example.com'
+    page.click("text=Alice (alice@example.com)")
+
+    # The virtual browser acts just like a normal browser and goes to the next
+    # page without us having to tell it to.
+
+    # Then we look for specific test classes that we have put into the HTML
+    # as targets for our tests to look for. This one is called `t-title`.
+    # You can see it in `templates/books/show.html`
+    title_element = page.locator(".t-name")
+    expect(title_element).to_have_text("Name: Alice")
+
+    # We do the same for the author name
+    author_element = page.locator(".t-email")
+    expect(author_element).to_have_text("Email: alice@example.com")
+
+
+"""
+When we create a new user
+We see it in the users index
+"""
+def test_create_user(db_connection, page, test_web_address):
+    db_connection.seed("seeds/makers_bnb.sql")
+    page.goto(f"http://{test_web_address}/users")
+
+    # This time we click the link with the text 'Add a new user'
+    page.click("text=Add a new user")
+
+    # Then we fill out the field with the name attribute 'name'
+    page.fill("input[name='name']", "Charlie")
+
+    # And the field with the name attribute 'email'
+    page.fill("input[name='email']", "charlie@example.com")
+
+    # Finally we click the button with the text 'Create User'
+    page.click("text=Create User")
+
+    # Just as before, the virtual browser acts just like a normal browser and
+    # goes to the next page without us having to tell it to.
+
+    title_element = page.locator(".t-name")
+    expect(title_element).to_have_text("Name: Charlie")
+
+    author_element = page.locator(".t-email")
+    expect(author_element).to_have_text("Email: charlie@example.com")
