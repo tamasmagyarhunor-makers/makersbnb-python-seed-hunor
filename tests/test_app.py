8d551d76ae0c8d91c46284cc12a52660b921fdcb
eanna durham <eannadurham@gmail.com>
2025-07-23 15:00:41 +0100

    added space repository and start of route testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,19 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(p_tag).to_have_text("This is the homepage.")
+
+"""
+When I call GET /spaces
+I get a list of all the spaces
+"""
+
+def test_get_spaces(db_connection, test_web_address, page):
+    db_connection.seed("seeds/makers_bnb.sql")
+    page.goto(f"http://{test_web_address}/spaces")
+    h1_tag = page.locator("h1")
+    expect(h1_tag).to_have_text("All Spaces")
+
+    # assert response.status_code == 200
+    # assert "<h1>All Spaces</h1>" in html
+    # assert "Cozy london flat" in html
+    # assert "£85.0" in html
