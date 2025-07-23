9f339c8a34144404d13a6af67951645a6cce20f6
emma <e.ferrari@live.co.uk>
2025-07-23 13:13:54 +0100

    create() in SpaceRepository complete

--- lib/space_repository.py
+++ lib/space_repository.py
@@ -9,11 +9,13 @@ class SpaceRepository():
         rows = self._connection.execute('SELECT * FROM spaces;')
         spaces = []
         for row in rows:
-            item = Space(row["id"], row["name"], row["description"], row['price_per_night'])
+            item = Space(row["id"], row["name"], row["description"], row['price_per_night'], row['user_id'])
             spaces.append(item)
         return spaces
 
-
-
-
+    def create(self, space):
+        self._connection.execute(
+            'INSERT INTO spaces (name, description, price_per_night, user_id) VALUES (%s, %s, %s, %s)',
+            [space.name, space.description, space.price_per_night, space.user_id]
+        )
 
--- tests/test_space.py
+++ tests/test_space.py
@@ -5,20 +5,21 @@ Space constructs with id, name, description and price_per_night
 """
 
 def test_space_constructs():
-    space = Space(1, 'Test Space', 'Test Description', 100.00)
+    space = Space(1, 'Test Space', 'Test Description', 100.00, 2)
     assert space.id == 1
     assert space.name == 'Test Space'
     assert space.description == 'Test Description'
     assert space.price_per_night == 100.00
+    assert space.user_id == 2
 
 """
 We can format spaces to strings
 """
 def test_spaces_format():
-    space = Space(1, 'Test Name', 'Test Description', 100.0)
-    assert str(space) == 'Space(1, Test Name, Test Description, 100.0)'
+    space = Space(1, 'Test Name', 'Test Description', 100.0, 2)
+    assert str(space) == 'Space(1, Test Name, Test Description, 100.0, 2)'
 
 def test_spaces_are_equal():
-    space1 = Space(1, 'Test Name', 'Test Description', 100.0)
-    space2 = Space(1, 'Test Name', 'Test Description', 100.0)
+    space1 = Space(1, 'Test Name', 'Test Description', 100.0, 2)
+    space2 = Space(1, 'Test Name', 'Test Description', 100.0, 2)
     assert space1 == space2
